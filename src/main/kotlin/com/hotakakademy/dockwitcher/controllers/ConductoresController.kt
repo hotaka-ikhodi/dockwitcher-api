7c08a9f14c1ec33e49046cd07a21abadf6ac63f3
111563_Moyano_Sergio_Ariel <63618103+sergioo22@users.noreply.github.com>
2024-07-11 11:06:01 -0300

    Update ConductoresController.kt
     Se agregan RestController y CrossOrigin

--- src/main/kotlin/com/hotakakademy/dockwitcher/controllers/ConductoresController.kt
+++ src/main/kotlin/com/hotakakademy/dockwitcher/controllers/ConductoresController.kt
@@ -8,11 +8,11 @@ import com.hotakakademy.dockwitcher.domain.services.ConductorService
 import com.hotakakademy.dockwitcher.domain.services.IConductorService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.*
 
 @Controller
+@CrossOrigin(origins = ["http://localhost:5173"])
+@RestController
 class ConductoresController (
         private val repository: IConductorRepository,
         private val conductorService: IConductorService
