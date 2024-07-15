3c0373fd1b92172d90eafe879ff5189dbe78f97c
sbermudez <sergio@hotaka.io>
2024-07-15 17:25:04 +0200

    Ejemplo de paginacion en conductores

--- src/main/kotlin/com/hotakakademy/dockwitcher/controllers/ConductoresController.kt
+++ src/main/kotlin/com/hotakakademy/dockwitcher/controllers/ConductoresController.kt
@@ -2,11 +2,11 @@ package com.hotakakademy.dockwitcher.controllers
 
 import com.hotakakademy.dockwitcher.contracts.ConductorDto
 import com.hotakakademy.dockwitcher.domain.entities.Conductor
-import com.hotakakademy.dockwitcher.domain.factories.IConductorFactory
 import com.hotakakademy.dockwitcher.domain.repositories.IConductorRepository
-import com.hotakakademy.dockwitcher.domain.services.ConductorService
 import com.hotakakademy.dockwitcher.domain.services.IConductorService
-import org.springframework.stereotype.Controller
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
@@ -16,8 +16,13 @@ class ConductoresController (
         private val conductorService: IConductorService
 ) {
     @GetMapping("/conductores")
-    fun listado(): List<Conductor>{
-        return repository.findAll()
+    fun listado(@RequestParam(defaultValue = "0") page: Int,
+                @RequestParam(defaultValue = "10") size: Int,
+                @RequestParam(defaultValue = "date") sort: String,
+                @RequestParam(defaultValue = "desc") direction: String
+    ): Page<Conductor>{
+        val paging = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(direction), sort))
+        return repository.findAll(paging)
     }
 
     @GetMapping("/conductores/nuevo")
--- src/main/kotlin/com/hotakakademy/dockwitcher/domain/repositories/ConductorRepository.kt
+++ src/main/kotlin/com/hotakakademy/dockwitcher/domain/repositories/ConductorRepository.kt
@@ -2,9 +2,11 @@ package com.hotakakademy.dockwitcher.domain.repositories
 
 import com.hotakademy.dockwitcher.domain.repositories.AbstractMongoRepository
 import com.hotakakademy.dockwitcher.domain.entities.Conductor
-import com.hotakakademy.dockwitcher.domain.entities.Tractora
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.support.PageableExecutionUtils
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -12,8 +14,11 @@ class ConductorRepository(
         @Qualifier("mongoMasterTemplate")
         mongoTemplate: MongoTemplate)
     : AbstractMongoRepository<Conductor>(mongoTemplate, Conductor::class.java), IConductorRepository {
-    override fun findAll(): List<Conductor> {
-        val list = mongoTemplate.findAll(Conductor::class.java)
-        return list
+    override fun findAll(paging: PageRequest): Page<Conductor> {
+        val query = org.springframework.data.mongodb.core.query.Query()
+        val count = mongoTemplate.count(query, Conductor::class.java)
+        query.with(paging)
+        val list = mongoTemplate.find(query, Conductor::class.java)
+        return PageableExecutionUtils.getPage(list, paging, { count })
     }
 }
