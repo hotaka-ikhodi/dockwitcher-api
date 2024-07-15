4e17df163190ecc4a116d891ee6fffa56513f47f
111563_Moyano_Sergio_Ariel <63618103+sergioo22@users.noreply.github.com>
2024-07-15 11:35:15 -0300

    cambio de carpeta corsConfig
    se cambia de la carpeta kotlin a java

--- src/main/java/com/hotakademy/dockwitcher/configuration/CorsConfig.java
+++ src/main/java/com/hotakademy/dockwitcher/configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.hotakakademy.dockwitcher.configuration;
+package com.hotakademy.dockwitcher.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- src/main/kotlin/com/hotakakademy/dockwitcher/controllers/ConductoresController.kt
+++ src/main/kotlin/com/hotakakademy/dockwitcher/controllers/ConductoresController.kt
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
-@Controller
-@CrossOrigin(origins = ["http://localhost:5173"])
 @RestController
 class ConductoresController (
         private val repository: IConductorRepository,
