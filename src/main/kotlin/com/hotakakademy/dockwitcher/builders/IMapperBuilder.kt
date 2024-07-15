29abde03020a37c1a5691e9431111bf1cf6f7682
maximofloress <95429@sistemas.frc.utn.edu.ar>
2024-07-15 11:54:59 -0300

    Component al mapperBuilder

--- src/main/kotlin/com/hotakakademy/dockwitcher/builders/IMapperBuilder.kt
+++ src/main/kotlin/com/hotakakademy/dockwitcher/builders/IMapperBuilder.kt
@@ -5,8 +5,9 @@ import org.modelmapper.Converter
 import org.modelmapper.ModelMapper
 import org.modelmapper.config.Configuration
 import org.modelmapper.convention.MatchingStrategies
+import org.springframework.stereotype.Component
 
-
+@Component
 class MapperBuilder {
     fun build(): ModelMapper {
         val modelMapper = ModelMapper()
